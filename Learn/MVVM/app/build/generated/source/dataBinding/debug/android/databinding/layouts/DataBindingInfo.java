ea5c643fa87a78a2129eb75c1f797e052ac790b9
楊政斌 <chugermobin@yangzhengbinde-MacBook-Pro.local>
2017-03-11 22:56:08 +0800

    delete MVVM

--- Learn/MVVM/app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="860ac008-3cb1-4645-b887-7148300b6318", modulePackage="com.twcts.mvvm", sdkRoot="/Users/chugermobin/Library/Android/sdk",layoutInfoDir="/Users/chugermobin/Downloads/MVVM/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=21,enableDebugLogs=false,printEncodedError=true)
-public class DataBindingInfo {}
